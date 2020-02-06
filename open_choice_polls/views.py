c213ea121df4fde5847c92fc800fd74afccddf41
Robert Habermann <mail@rhab.de>
2020-02-06 20:12:34 +0100

    fix sort order

--- open_choice_polls/views.py
+++ open_choice_polls/views.py
@@ -1,6 +1,7 @@
 import re
 
-from django.core.exceptions import MultipleObjectsReturned
+from django.core.exceptions import MultipleObjectsReturned#
+from django.db.models.functions import Lower
 from django.utils.translation import gettext_lazy as _
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, redirect
