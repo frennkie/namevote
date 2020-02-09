91f0f37b38504c30c8bf5c13eae493bde2e33904
Robert Habermann <mail@rhab.de>
2020-02-09 11:58:19 +0100

    use django builtin get_randon_string

--- open_choice_polls/models.py
+++ open_choice_polls/models.py
@@ -1,7 +1,5 @@
 import re
-from random import random as dist_random
 import uuid
-
 
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -10,35 +8,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.urls import reverse
 from django.utils import timezone
+from django.utils.crypto import get_random_string
 from django.utils.safestring import mark_safe
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
-from itertools import accumulate as _accumulate, repeat as _repeat
-from bisect import bisect as _bisect
 
-
-def choices(population, weights=None, *, cum_weights=None, k=1):
-    """Return a k sized list of population elements chosen with replacement.
-    If the relative weights or cumulative weights are not specified,
-    the selections are made with equal probability.
-    """
-    random = dist_random
-    n = len(population)
-    if cum_weights is None:
-        if weights is None:
-            _int = int
-            n += 0.0  # convert to float for a small speed improvement
-            return [population[_int(random() * n)] for i in _repeat(None, k)]
-        cum_weights = list(_accumulate(weights))
-    elif weights is not None:
-        raise TypeError('Cannot specify both weights and cumulative weights')
-    if len(cum_weights) != n:
-        raise ValueError('The number of weights does not match the population')
-    bisect = _bisect
-    total = cum_weights[-1] + 0.0  # convert to float
-    hi = n - 1
-    return [population[bisect(cum_weights, random() * total, 0, hi)]
-            for i in _repeat(None, k)]
+SELECTED_LETTERS = 'ABCDEFGHKMNPQRSTUVWX'
+SELECTED_NUMBERS = '23456789'
 
 
 class Voter(models.Model):
@@ -57,16 +33,16 @@ class Voter(models.Model):
 
     @staticmethod
     def create_enrollment_code():
-        first = ''.join(choices('ABCDEFGHKMNPQRSTUVWX', k=2))
-        second = ''.join(choices('23456789', k=3))
-        third = ''.join(choices('abcdefghkmnpqrstuvwx', k=2))
+        first = get_random_string(2, SELECTED_LETTERS)
+        second = get_random_string(3, SELECTED_NUMBERS)
+        third = get_random_string(2, SELECTED_LETTERS.lower())
         return "{}{}{}".format(first, second, third)
 
     @staticmethod
     def create_new_password():
-        first = ''.join(choices('ABCDEFGHKMNPQRSTUVWX', k=4))
-        second = ''.join(choices('23456789', k=5))
-        third = ''.join(choices('abcdefghkmnpqrstuvwx', k=4))
+        first = get_random_string(4, SELECTED_LETTERS)
+        second = get_random_string(5, SELECTED_NUMBERS)
+        third = get_random_string(4, SELECTED_LETTERS.lower())
         return "{}-{}-{}".format(first, second, third)
 
     def __repr__(self):
