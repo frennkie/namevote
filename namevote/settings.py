0e09c6660d07f8922a30255738964b110528830a
Robert Habermann <mail@rhab.de>
2020-02-10 21:09:24 +0100

    stuff

--- namevote/settings.py
+++ namevote/settings.py
@@ -91,9 +91,9 @@ LOGGING = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
