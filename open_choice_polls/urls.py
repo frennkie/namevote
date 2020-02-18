99b18e182a891861685c51bc3aedd372f2b6f00f
Robert Habermann <mail@rhab.de>
2020-02-18 12:21:36 +0100

    change login (separate CBVs)

--- open_choice_polls/urls.py
+++ open_choice_polls/urls.py
@@ -7,20 +7,18 @@ app_name = 'open_choice_polls'
 urlpatterns = [
     path('', views.QuestionListView.as_view(), name='question-list'),
 
-    path('voter/selogin/', views.selogin, name='se-login'),
-    path('voter/selogin/<username>/', views.selogin, name='se-login-username'),
-    path('voter/selogin-user/', views.selogin_user, name='se-login-user'),
-
-    path('voter/logout/', auth_views.LogoutView.as_view(), name='logout'),
-
     path('voter/password_change/', auth_views.PasswordChangeView.as_view(), name='password-change'),
     path('voter/password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password-change-done'),
-
     path('voter/password_reset/', auth_views.PasswordResetView.as_view(), name='password-reset'),
     path('voter/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password-reset-done'),
     path('voter/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
     path('voter/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password-reset-complete'),
 
+    path('voter/enroll/', views.EnrollFormView.as_view(), name='voter-enroll'),
+    path('voter/logout/', auth_views.LogoutView.as_view(), name='voter-logout'),
+    path('voter/sign_in/', views.SignInFormView.as_view(), name='voter-sign-in'),
+
+    # leave as last as it potentially shadows the other voter/ path
     path('voter/<username>/', views.VoterDetailView.as_view(), name='voter-detail'),
 
     path('<slug:slug>,<uuid:id>/', views.QuestionDetailView.as_view(), name='question-detail'),
